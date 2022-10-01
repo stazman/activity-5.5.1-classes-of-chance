45a59eeca501a39161792868d914488e3c7c75a5
Christopher DiStasio <cadistasio@gmail.com>
2022-10-01 13:57:47 -0500

    Partially completes activity

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin)
+    {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.winnings = 0;
+        this.isFakeCoin = false;
+    }
+
+    playGame(betAmount){
+
+        if ((Math.random() <= 0.5) || this.isFakeCoin == true) {
+            console.log(`${this.name} "wins!"`);
+        }
+        else {
+            this.winnings = betAmount * (this.timesPlayed + 1);
+            console.log(`Player wins!"`);
+            console.log(this.winnings);
+        }
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", true);
 console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+myCasino.playGame(5, true);
+// myCasino.playGame(15);
+// myCasino.playGame(25);
+// myCasino.playGame(35);
 
 // BONUS TESTS
 /*
